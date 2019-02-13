9b8880e6a1ce7cec18d972ab99570d0c3990fcca
Masayuki Goto <originals0711@gmail.com>
2019-02-14 08:15:35 +0900

    fix

--- Formula/docui.rb
+++ Formula/docui.rb
@@ -3,11 +3,19 @@ class Docui < Formula
   homepage "https://github.com/skanehira/docui"
   url "https://github.com/skanehira/docui/archive/1.0.2.tar.gz"
   sha256 "0c55d5261bda1fd4bf070541357482be0baa83043651179c8a38129b5ff8a0a4"
+  depends_on "go" => :build
 
   def install
+    print buildpath
     ENV['GOPATH'] = buildpath
+    docui_path = buildpath/"src/github.com/skanehira/docui"
+    docui_path.install buildpath.children
     system 'go', 'install'
     bin.install 'docui'
+    cd docui_path do
+      system "go", "build"
+      bin.install "docui"
+    end
   end
 
   test do

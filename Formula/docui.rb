df9bb14764f006f7d126b76e9e611ba98bc3e6d3
Masayuki Goto <originals0711@gmail.com>
2019-02-14 07:42:48 +0900

    fix install process

--- Formula/docui.rb
+++ Formula/docui.rb
@@ -5,11 +5,7 @@ class Docui < Formula
   sha256 "0c55d5261bda1fd4bf070541357482be0baa83043651179c8a38129b5ff8a0a4"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    bin.install "docui"
   end
 
   test do

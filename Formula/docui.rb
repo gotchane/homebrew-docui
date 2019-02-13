1149051e591ed1aab05bdcb389f9f25936de1c2c
Masayuki Goto <originals0711@gmail.com>
2019-02-14 08:07:17 +0900

    fix

--- Formula/docui.rb
+++ Formula/docui.rb
@@ -5,11 +5,9 @@ class Docui < Formula
   sha256 "0c55d5261bda1fd4bf070541357482be0baa83043651179c8a38129b5ff8a0a4"
 
   def install
-    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
-    (buildpath/"src/github.com/skanehira/docui").install buildpath.children
-    cd "src/github.com/skanehira/docui" do
-      system "go", "build", "-o", bin/"goreleaser"
-    end
+    ENV['GOPATH'] = buildpath
+    system 'go', 'build', '-o', 'docui'
+    bin.install 'docui'
   end
 
   test do

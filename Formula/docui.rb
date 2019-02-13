ed890a660aa4d911d7708adbcc969e302a3e7d37
Masayuki Goto <originals0711@gmail.com>
2019-02-14 08:20:22 +0900

    fix

--- Formula/docui.rb
+++ Formula/docui.rb
@@ -13,7 +13,7 @@ class Docui < Formula
     system 'go', 'install'
     bin.install 'docui'
     cd docui_path do
-      system "go", "install"
+      system "go", "install", "main.go"
       bin.install "docui"
     end
   end

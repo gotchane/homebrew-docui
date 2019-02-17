483251ba75e9849f12b38974994399eef0abd3b4
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-18 07:36:54 +0900

    Brew formula update for docui version 1.0.7

--- Formula/docui.rb
+++ Formula/docui.rb
@@ -2,9 +2,9 @@
 class Docui < Formula
   desc "TUI Client for Docker"
   homepage "https://github.com/skanehira/docui"
-  url "https://github.com/gotchane/docui/releases/download/1.0.6/docui_1.0.6_Darwin_x86_64.tar.gz"
-  version "1.0.6"
-  sha256 "09373d147a12c1a3fc073aeecefe400a61ef59caeeca663ca7e8677b63eb2343"
+  url "https://github.com/gotchane/docui/releases/download/1.0.7/docui_1.0.7_Darwin_x86_64.tar.gz"
+  version "1.0.7"
+  sha256 "78f2ddae96ed3c272b8d17409eb054c372c86b9d8c992cc787f8e3ed3cbbd28b"
   
   depends_on "go"
 
